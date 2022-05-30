919c1d6921343b9015b92ba59457a055db612f19
Sai Chaitanya Aluru <alurusaichaitanya@gmail.com>
2022-05-30 19:06:55 +0800

    feat: added roll feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftNumber = 1;
+  var rightNumber = 2;
+
+  void changeDiceFace() {
+    setState(() {
+      leftNumber = Random().nextInt(6) + 1;
+      rightNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Row(
+      children: <Widget>[
+        Expanded(
+            child: TextButton(
+                onPressed: () {
+                  changeDiceFace();
+                },
+                child: Image(
+                  image: AssetImage('images/dice$leftNumber.png'),
+                ))),
+        Expanded(
+            child: TextButton(
+          onPressed: () {
+            changeDiceFace();
+          },
+          child: Image.asset('images/dice$rightNumber.png'),
+        )),
+      ],
+    ));
   }
 }
